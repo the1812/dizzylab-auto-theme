ffbba627df445c00600f4a72a89972abf739b780
the1812 <the1812@outlook.com>
2021-03-20 16:34:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# dizzylab auto theme
+[dizzylab](https://www.dizzylab.net/) 自适应主题, 跟随系统亮/暗设定.
+
+![image](./dizzylab-auto-theme.jpg)
+
+## 安装
+> 需要浏览器拥有 [Stylus](https://github.com/openstyles/stylus) 或其他同类插件.
+
+[点击此处](https://cdn.jsdelivr.net/gh/the1812/dizzylab-auto-theme@master/dizzylab.user.css)安装.
---
+++ build.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const code = fs.readFileSync('dizzylab.css', { encoding: 'utf-8'})
+const style = `
+/* ==UserStyle==
+@name        dizzylab auto theme
+@description dizzylab 自适应主题
+@namespace   the1812
+@version     1.0.0
+@author      Grant Howard
+@homepageURL https://github.com/the1812/dizzylab-auto-theme/
+@supportURL  https://github.com/the1812/dizzylab-auto-theme/issues
+@updateURL   https://cdn.jsdelivr.net/gh/the1812/dizzylab-auto-theme@master/dizzylab.user.css
+@license     MIT
+==/UserStyle== */
+
+@-moz-document domain("www.dizzylab.net") {
+  ${code}
+}
+`.trim()
+fs.writeFileSync('dizzylab.user.css', style)
